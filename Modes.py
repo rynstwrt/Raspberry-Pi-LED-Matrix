d85f17901a2409a5fdd52d0d90d6f24dc0ba8404
Ryan Stewart <ryanstewartalex@gmail.com>
2021-08-11 22:06:34 -0500

    Update Modes.py

--- Modes.py
+++ Modes.py
@@ -8,10 +8,9 @@ import requests
 from math import ceil
 import pyaudio
 import numpy as np
+
 np.set_printoptions(suppress=True)
 from scipy import fft, arange
-
-
 
 ################### VARIABLES ###################
 current_function = None
@@ -21,8 +20,6 @@ COLOR_BLACK = (0, 0, 0)
 NUM_ROWS = None
 NUM_COLS = None
 
-
-
 ################### AUDIO VARIABLES ###################
 AUDIO_DEVICE_INDEX = 0
 AUDIO_FORMAT = pyaudio.paInt16
@@ -30,22 +27,19 @@ AUDIO_CHANNELS = 2
 AUDIO_RATE = 48000
 AUDIO_BLOCK_SIZE = 1024 * 2
 
-
-
 ################### OBJECT DETECTION VARIABLES ###################
 PATH_TO_CKPT = "/home/pi/Sample_TFLite_model/detect.tflite"
 PATH_TO_LABELS = "/home/pi/Sample_TFLite_model/labelmap.txt"
 
 
-
 ################### HELPER FUNCTIONS ###################
-def init(led_pin, num_leds, brightness, num_rows, num_cols, auto_write = True):
+def init(led_pin, num_leds, brightness, num_rows, num_cols, auto_write=True):
     global pixels
     global NUM_LEDS
     global NUM_ROWS
     global NUM_COLS
 
-    pixels = neopixel.NeoPixel(led_pin, num_leds, brightness = brightness / 100, auto_write = auto_write)
+    pixels = neopixel.NeoPixel(led_pin, num_leds, brightness=brightness / 100, auto_write=auto_write)
     NUM_LEDS = num_leds
     NUM_ROWS = num_rows
     NUM_COLS = num_cols
@@ -86,33 +80,31 @@ def get_pixel_y(index):
 
 def get_pixel_index_at(x, y):
     a = (NUM_COLS - 1 - x) * NUM_ROWS
-    b = NUM_ROWS - y - 1 if x % 2 != 0 else y 
+    b = NUM_ROWS - y - 1 if x % 2 != 0 else y
     return (a + b)
-
 
 
 ################### MODE FUNCTIONS ###################
 def dual_stripes(color1, color2):
     blank()
     while True:
-        if current_function != None and current_function != dual_stripes:
+        if current_function is not None and current_function != dual_stripes:
             return
 
         for i in range(NUM_LEDS):
-            if current_function != None and current_function != dual_stripes:
+            if current_function is not None and current_function != dual_stripes:
                 return
 
             pixels[i] = color1
             pixels.show()
 
         for i in range(NUM_LEDS):
-            if current_function != None and current_function != dual_stripes:
+            if current_function is not None and current_function != dual_stripes:
                 return
 
             pixels[i] = color2
             pixels.show()
 
-        
 
 def rainbow_cycle():
     while True:
@@ -121,7 +113,7 @@ def rainbow_cycle():
 
         blank()
 
-        for j in range(255):   
+        for j in range(255):
             for i in range(NUM_LEDS):
                 if current_function != None and current_function != rainbow_cycle:
                     return
@@ -132,24 +124,22 @@ def rainbow_cycle():
             sleep(2)
 
 
-
 def theater_chase_rainbow(wait_ms=50):
     for j in range(256):
         for q in range(3):
             for i in range(0, NUM_LEDS, 3):
-                pixels[i+q - 1] = wheel((i+j) % 255)
+                pixels[i + q - 1] = wheel((i + j) % 255)
 
                 if current_function != None and current_function != theater_chase_rainbow:
                     return
 
             pixels.show()
-            sleep(wait_ms/1000.0)
+            sleep(wait_ms / 1000.0)
             for i in range(0, NUM_LEDS, 3):
                 if current_function != None and current_function != theater_chase_rainbow:
                     return
-                pixels[i+q - 1] = (0, 0, 0)
+                pixels[i + q - 1] = (0, 0, 0)
     theater_chase_rainbow(wait_ms)
-
 
 
 def load_image_url(url):
@@ -164,16 +154,14 @@ def load_image_url(url):
 
             coords = x, y
             pixels[index] = img.getpixel(coords)
-    
+
     pixels.show()
 
 
-
-def load_image_camera(img_array, reverse = True):
+def load_image_camera(img_array, reverse=True):
     img = Image.fromarray(img_array).convert("RGB").resize((NUM_COLS, NUM_ROWS), Image.ANTIALIAS)
     r, g, b = img.split()
     img = Image.merge("RGB", (b, g, r))
-
 
     for x in range(NUM_COLS):
         for y in range(NUM_ROWS):
@@ -184,9 +172,8 @@ def load_image_camera(img_array, reverse = True):
 
             coords = NUM_COLS - x - 1 if reverse else x, y
             pixels[index] = img.getpixel(coords)
-    
-    pixels.show()
 
+    pixels.show()
 
 
 def load_camera():
@@ -203,9 +190,8 @@ def load_camera():
             return
 
         load_image_camera(img)
-    
-    video_capture.release()
 
+    video_capture.release()
 
 
 def text(passed_text, color):
@@ -217,7 +203,7 @@ def text(passed_text, color):
     width = text_size[0]
     height = text_size[1]
 
-    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), passed_text, color, font = font)
+    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), passed_text, color, font=font)
 
     for x in range(NUM_COLS):
         for y in range(NUM_ROWS):
@@ -228,9 +214,8 @@ def text(passed_text, color):
 
             coords = x, y
             pixels[index] = img.getpixel(coords)
-    
-    pixels.show()
 
+    pixels.show()
 
 
 def rainbow_text(text):
@@ -244,7 +229,7 @@ def rainbow_text(text):
     width = text_size[0]
     height = text_size[1]
 
-    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), text, (255, 255, 255), font = font)
+    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), text, (255, 255, 255), font=font)
 
     while True:
         if current_function != None and current_function != rainbow_text:
@@ -255,16 +240,14 @@ def rainbow_text(text):
                 pixel_index = get_pixel_index_at(x, y)
                 coords = x, y
 
-                color = COLOR_BLACK if img.getpixel(coords) == COLOR_BLACK else wheel(index + pixel_index & 255) 
+                color = COLOR_BLACK if img.getpixel(coords) == COLOR_BLACK else wheel(index + pixel_index & 255)
                 pixels[pixel_index] = color
 
-        
         pixels.show()
         index += 2
 
         if index > 255:
             index = 0
-
 
 
 def scrolling_text(text, color):
@@ -284,20 +267,19 @@ def scrolling_text(text, color):
         width = text_size[0]
         height = text_size[1]
 
-        draw.text((-index, ceil(NUM_ROWS / 2 - height / 2) - 1), text, color, font = font)
+        draw.text((-index, ceil(NUM_ROWS / 2 - height / 2) - 1), text, color, font=font)
 
         for x in range(NUM_COLS):
             for y in range(NUM_ROWS):
                 pixel_index = get_pixel_index_at(x, y)
                 coords = x, y
                 pixels[pixel_index] = img.getpixel(coords)
-        
+
         pixels.show()
         index += 1
 
         if index > width:
             index = -NUM_COLS
-
 
 
 def display_time(time_string, color):
@@ -309,7 +291,7 @@ def display_time(time_string, color):
     width = text_size[0]
     height = text_size[1]
 
-    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), time_string, color, font = font)
+    draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), time_string, color, font=font)
 
     for x in range(NUM_COLS):
         for y in range(NUM_ROWS):
@@ -320,11 +302,10 @@ def display_time(time_string, color):
 
             coords = x, y
             pixels[index] = img.getpixel(coords)
-    
+
     pixels.show()
 
     sleep(.02)
-
 
 
 def get_time():
@@ -342,7 +323,6 @@ def get_time():
     return "{}:{}".format(h, m)
 
 
-
 def clock(color):
     local_time = localtime()
     h = int(strftime("%H", local_time))
@@ -351,10 +331,10 @@ def clock(color):
 
     if h > 12:
         h = h - 12
-    
+
     if h < 10 and len(str(h)) == 1:
         h = "0{}".format(h)
-    
+
     display_time("{}:{}".format(h, m), color)
     sleep(60 - int(s))
 
@@ -370,13 +350,12 @@ def clock(color):
         display_time(get_time(), color)
 
 
-
 def sparkle(color):
     while True:
         for i in range(NUM_LEDS):
             if current_function != None and current_function != sparkle:
                 break
-            
+
             pixel_color = color if i % 2 == 0 else COLOR_BLACK
             pixels[i] = pixel_color
 
@@ -394,13 +373,14 @@ def sparkle(color):
         sleep(.2)
 
 
-
 def temperature(city, state, country, api_key, units):
     while True:
         if current_function != None and current_function != temperature:
             return
 
-        response = requests.get("http://api.openweathermap.org/data/2.5/weather?q={},{},{}&appid={}&units={}".format(city, state, country, api_key, units))
+        response = requests.get(
+            "http://api.openweathermap.org/data/2.5/weather?q={},{},{}&appid={}&units={}".format(city, state, country,
+                                                                                                 api_key, units))
         if (response.status_code != 200):
             pixels.fill((255, 0, 0))
             pixels.show()
@@ -426,7 +406,8 @@ def temperature(city, state, country, api_key, units):
         width = text_size[0]
         height = text_size[1]
 
-        draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), temperature_text, color, font = font)
+        draw.text((ceil(NUM_COLS / 2 - width / 2), ceil(NUM_ROWS / 2 - height / 2) - 1), temperature_text, color,
+                  font=font)
 
         for x in range(NUM_COLS):
             for y in range(NUM_ROWS):
@@ -437,17 +418,15 @@ def temperature(city, state, country, api_key, units):
 
                 coords = x, y
                 pixels[index] = img.getpixel(coords)
-        
-        pixels.show()
-        sleep(320) # 5 minutes
 
+        pixels.show()
+        sleep(320)  # 5 minutes
 
 
 def load_image_video(img_array):
     img = Image.fromarray(img_array).convert("RGB").resize((NUM_COLS, NUM_ROWS), Image.ANTIALIAS)
     r, g, b = img.split()
     img = Image.merge("RGB", (b, g, r))
-
 
     for x in range(NUM_COLS):
         for y in range(NUM_ROWS):
@@ -458,9 +437,8 @@ def load_image_video(img_array):
 
             coords = x, y
             pixels[index] = img.getpixel(coords)
-    
-    pixels.show()
 
+    pixels.show()
 
 
 def load_video_url(url):
@@ -475,21 +453,20 @@ def load_video_url(url):
 
         if not success:
             return
-        
-        load_image_video(img)
 
+        load_image_video(img)
 
 
 def equalizer(color):
     audio = pyaudio.PyAudio()
 
     stream = audio.open(
-        format = AUDIO_FORMAT, 
-        channels = AUDIO_CHANNELS,
-        rate = AUDIO_RATE,
-        input_device_index = AUDIO_DEVICE_INDEX,
-        input = True,
-        frames_per_buffer = AUDIO_BLOCK_SIZE)
+        format=AUDIO_FORMAT,
+        channels=AUDIO_CHANNELS,
+        rate=AUDIO_RATE,
+        input_device_index=AUDIO_DEVICE_INDEX,
+        input=True,
+        frames_per_buffer=AUDIO_BLOCK_SIZE)
 
     while True:
         if current_function != None and current_function != equalizer:
@@ -498,7 +475,7 @@ def equalizer(color):
             audio.terminate()
             return
 
-        data = np.fromstring(stream.read(AUDIO_BLOCK_SIZE, exception_on_overflow = False), dtype = np.int16)
+        data = np.fromstring(stream.read(AUDIO_BLOCK_SIZE, exception_on_overflow=False), dtype=np.int16)
 
         data = data - np.average(data)
 
@@ -506,7 +483,7 @@ def equalizer(color):
         k = arange(n)
 
         tarr = n / float(AUDIO_RATE)
-        
+
         frqarr = k / float(tarr)
         frqarr = frqarr[range(n // 2)]
 
@@ -522,7 +499,7 @@ def equalizer(color):
 
             if i == len(chunked_data) - 1:
                 y_value //= 5
-            
+
             y_values.append(y_value)
 
         for x in range(len(y_values)):
@@ -530,21 +507,20 @@ def equalizer(color):
 
             for y in range(NUM_ROWS):
                 pixels[get_pixel_index_at(x, NUM_ROWS - y - 1)] = color if y <= value else COLOR_BLACK
-        
-        pixels.show()
 
+        pixels.show()
 
 
 def rainbow_equalizer():
     audio = pyaudio.PyAudio()
 
     stream = audio.open(
-        format = AUDIO_FORMAT, 
-        channels = AUDIO_CHANNELS,
-        rate = AUDIO_RATE,
-        input_device_index = AUDIO_DEVICE_INDEX,
-        input = True,
-        frames_per_buffer = AUDIO_BLOCK_SIZE)
+        format=AUDIO_FORMAT,
+        channels=AUDIO_CHANNELS,
+        rate=AUDIO_RATE,
+        input_device_index=AUDIO_DEVICE_INDEX,
+        input=True,
+        frames_per_buffer=AUDIO_BLOCK_SIZE)
 
     while True:
         if current_function != None and current_function != rainbow_equalizer:
@@ -553,7 +529,7 @@ def rainbow_equalizer():
             audio.terminate()
             return
 
-        data = np.fromstring(stream.read(AUDIO_BLOCK_SIZE, exception_on_overflow = False), dtype = np.int16)
+        data = np.fromstring(stream.read(AUDIO_BLOCK_SIZE, exception_on_overflow=False), dtype=np.int16)
 
         data = data - np.average(data)
 
@@ -561,7 +537,7 @@ def rainbow_equalizer():
         k = arange(n)
 
         tarr = n / float(AUDIO_RATE)
-        
+
         frqarr = k / float(tarr)
         frqarr = frqarr[range(n // 2)]
 
@@ -577,22 +553,23 @@ def rainbow_equalizer():
 
             if i == len(chunked_data) - 1:
                 y_value //= 5
-            
+
             y_values.append(y_value)
 
         for x in range(len(y_values)):
             value = y_values[x]
 
             for y in range(NUM_ROWS):
-                pixels[get_pixel_index_at(x, NUM_ROWS - y - 1)] = wheel(((x + y) * (255 // NUM_ROWS)) & 255) if y <= value else COLOR_BLACK
-        
+                pixels[get_pixel_index_at(x, NUM_ROWS - y - 1)] = wheel(
+                    ((x + y) * (255 // NUM_ROWS)) & 255) if y <= value else COLOR_BLACK
+
         pixels.show()
 
-    
 
 has_imported_obj_detection = False
-def object_detection(color, thresh =  0.45):
-    
+
+
+def object_detection(color, thresh=0.45):
     global has_imported_obj_detection
 
     if not has_imported_obj_detection:
@@ -605,9 +582,9 @@ def object_detection(color, thresh =  0.45):
         labels = [line.strip() for line in f.readlines()]
 
     if labels[0] == "???":
-        del(labels[0])
+        del (labels[0])
 
-    interpreter = Interpreter(model_path = PATH_TO_CKPT)
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -626,13 +603,13 @@ def object_detection(color, thresh =  0.45):
         frame = frame1.copy()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_resized = cv2.resize(frame_rgb, (width, height))
-        input_data = np.expand_dims(frame_resized, axis = 0)
+        input_data = np.expand_dims(frame_resized, axis=0)
 
         interpreter.set_tensor(input_details[0]["index"], input_data)
         interpreter.invoke()
 
         classes = interpreter.get_tensor(output_details[1]["index"])[0]
-        scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence 
+        scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence
 
         highest_score = 0
         current_label = None
@@ -649,7 +626,7 @@ def object_detection(color, thresh =  0.45):
         img = Image.new("RGB", (NUM_COLS, NUM_ROWS))
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("/user/share/fonts/truetype/noto/NotoMono-Regular.ttf", 10)
-        text_width = draw.textsize(current_label, font = font)[0]
+        text_width = draw.textsize(current_label, font=font)[0]
 
         x_pos = text_width + NUM_COLS
         while x_pos > -text_width - NUM_COLS:
@@ -661,7 +638,7 @@ def object_detection(color, thresh =  0.45):
             text_width = text_size[0]
             text_height = text_size[1]
 
-            draw.text((x_pos, ceil(NUM_ROWS / 2 - text_height / 2) - 1), current_label, color, font = font)
+            draw.text((x_pos, ceil(NUM_ROWS / 2 - text_height / 2) - 1), current_label, color, font=font)
 
             for x in range(NUM_COLS):
                 for y in range(NUM_ROWS):
@@ -672,9 +649,7 @@ def object_detection(color, thresh =  0.45):
 
                     coords = x, y
                     pixels[index] = img.getpixel(coords)
-            
+
             x_pos -= 1
 
             pixels.show()
-
-    
